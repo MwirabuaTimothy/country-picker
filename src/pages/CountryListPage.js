92c1234be405bd6dd6747d9405862aae12977fde
TechyTimo <techytimo@gmail.com>
2018-10-09 16:42:15 +0300

    reindented CountryListPage

--- src/pages/CountryListPage.js
+++ src/pages/CountryListPage.js
@@ -3,26 +3,21 @@ import PropTypes from 'prop-types'
 
 import CountryListItem from '../components/CountryListItem'
 
-const CountryListPage = ({
-  countries,
-  selectedCountry,
-  selectCountry,
-  searchFieldInput,
-  changeSearchFieldInput }) => (
+const CountryListPage = ({ countries, selectedCountry, selectCountry, searchFieldInput, changeSearchFieldInput }) => (
   <div>
-      <div className='input-group'>
+    <div className='input-group'>
       <span className='input-group-btn'>
-          <button className='btn btn-default' type='button'>Go!</button>
-        </span>
+        <button className='btn btn-default' type='button'>Go!</button>
+      </span>
       <input
-          type='text'
-          className='form-control'
-          value={searchFieldInput}
-          placeholder='client-search for countries'
-          onChange={e => changeSearchFieldInput(e.target.value)}
+        type='text'
+        className='form-control'
+        value={searchFieldInput}
+        placeholder='client-search for countries'
+        onChange={e => changeSearchFieldInput(e.target.value)}
       />
     </div>
-      { countries.length > 0
+    { countries.length > 0
       ? <div className='list-group list-scrollable'>
         {countries.map(country => (
           <CountryListItem
@@ -37,7 +32,7 @@ const CountryListPage = ({
       </div>
       : <div>No Countries match the search :(</div>
     }
-    </div>
+  </div>
 )
 
 CountryListPage.propTypes = {
